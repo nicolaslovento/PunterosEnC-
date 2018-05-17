35f746b8ef18de8f38140829ed1ab0375f2938cb
Nicolas <nicolaslovento.94@hotmail.com>
2018-05-17 10:24:58 -0300

    nomenclatura de punteros con enteros

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros: son direcciones donde se encuentra el dato. Necdesito saber donde empieza el dato guardado
+    int *punteroNumero;
+    int numero;
+    int numeroDos;
+
+    numero=66;
+    punteroNumero=&numero;
+    numeroDos=&numero;
+    //*punteroNumero=&numero; MAL
+    //*punteroNumero=55; MAL
+
+    printf("\nAsignacion de valor a- %d",numeroDos);
+    printf("\n%p",&numero);
+    printf("\n%p",punteroNumero);
+    printf("\n%d",*punteroNumero);
+    //printf("%d",*punteroNumero);
     return 0;
 }
